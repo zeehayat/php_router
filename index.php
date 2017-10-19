e0aafe9d41e6a3120656ddaa44dee98f461609e0
Zeeshan Hayat <zeenux@gmail.com>
2017-10-19 10:22:38 +0500

    Full Router

--- index.php
+++ index.php
@@ -15,4 +15,26 @@ function getCurrentUri()
         return $uri;
     }
 
-echo getCurrentUri();
+$routes = array();
+    $routes = explode('/', $base_url);
+    foreach($routes as $route)
+    {
+        if(trim($route) != '')
+            array_push($routes, $route);
+    }
+
+    /*
+    Now, $routes will contain all the routes. $routes[0] will correspond to first route.
+    For e.g. in above example $routes[0] is search, $routes[1] is book and $routes[2] is fitzgerald
+    */
+
+
+    
+    
+    if($routes[0] == “search”)
+    {
+        if($routes[1] == “book”)
+        {
+            searchBooksBy($routes[2]);
+        }
+    }
